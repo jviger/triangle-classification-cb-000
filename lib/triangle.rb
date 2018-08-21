19cdc55ae713e8e6caf0366fb88bc422754f74f8
Jason Viger <jason.viger@gmail.com>
2018-08-21 09:09:41 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,8 @@ class Triangle
   end
   
   def kind
-    if [side_1, side_2, side_3]
+    if [side_1, side_2, side_3].any? { |x| x <= 0}
+      
     if side_1 == side_2  && side_1 == side_3
       :equilateral
     elsif side_1 == side_2 || side_2 == side_3 || side_1 == side_3
