4410fe3fec1ce9ba2347c0113db639a1ea4f55c7
Jason Viger <jason.viger@gmail.com>
2018-08-21 09:12:49 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
   end
   
   def kind
-    if [side_1, side_2, side_3].any? { |x| x <= 0}
+    if [side_1, side_2, side_3].any? { |x| x <= 0} || side_1 + side_2 < side_3 || side_1 + side_3 < side_2 || side_2 + side 3 < side_1
       begin
         raise TriangleError
       rescue TriangleError => error
