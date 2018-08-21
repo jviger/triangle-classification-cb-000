7f52c3a4468db222e1409ece417a51917e311862
Jason Viger <jason.viger@gmail.com>
2018-08-21 09:04:13 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   
   def kind
     
-    if side_1 == side_2 == side_3
+    if side_1 == side_2  && side_1 == side_3
       :equilateral
     else if side_1 == side_2 || side_2 == side_3 || side_1 == side_3
       :isocelese
