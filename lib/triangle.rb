dc810e251c8997d682dc77c02a902032e9431ab0
Jason Viger <jason.viger@gmail.com>
2018-08-21 09:06:50 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
     if side_1 == side_2  && side_1 == side_3
       :equilateral
     elsif side_1 == side_2 || side_2 == side_3 || side_1 == side_3
-      :isocelese
+      :isosceles
     else 
       :scalene
     end
