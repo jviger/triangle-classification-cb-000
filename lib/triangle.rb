e45bc60bc21e509f9f93db22e5b25d252feb56cf
Jason Viger <jason.viger@gmail.com>
2018-08-21 09:11:18 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,14 +11,18 @@ class Triangle
   
   def kind
     if [side_1, side_2, side_3].any? { |x| x <= 0}
-      
-    if side_1 == side_2  && side_1 == side_3
-      :equilateral
-    elsif side_1 == side_2 || side_2 == side_3 || side_1 == side_3
-      :isosceles
-    else 
-      :scalene
-    end
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+          puts error.message
+      end
+    elsif side_1 == side_2  && side_1 == side_3
+        :equilateral
+      elsif side_1 == side_2 || side_2 == side_3 || side_1 == side_3
+        :isosceles
+      else 
+        :scalene
+      end
   end
   
   class TriangleError < StandardError
